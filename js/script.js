8c1785b1c6ddc9f88dce6e627fb4a41c6f16358d
Vlad <zhivoglad23@gmail.com>
2022-03-31 20:17:04 +0300

    first commit,made

--- js/script.js
+++ js/script.js
@@ -1,1 +1,30 @@
-console.log('hi');
+"use strict"
+
+
+const numberOfFilms = prompt("Сколько фильмов Вы посмотрели?","");
+//let answerConsole =  console.log(+numberOfFilms);
+
+
+let personalMovieDB = {
+	count: numberOfFilms,
+	movies: {
+	
+	},
+	actors:{
+
+	},
+	genres:[],
+	privat:false,
+};
+
+const a = prompt("Какой последний фильм вы смотрели?",""),
+ 		b = prompt("Какова ваша оценка?"),
+		c = prompt("Какой последний фильм вы смотрели?",""),
+		d = prompt("Какова ваша оценка?");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+
+console.log(personalMovieDB);
+
